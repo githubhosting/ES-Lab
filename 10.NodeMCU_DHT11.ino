6fe05c0b45780988c8472b4c19544c46c333f3a1
myselfshravan <myselfshravan@gmail.com>
2023-08-17 08:53:38 +0530

    added 9 and 10

--- 10.NodeMCU_DHT11.ino
+++ 10.NodeMCU_DHT11.ino
@@ -1,16 +1,14 @@
-#include <DHT.h>
-#define DHTPIN 2
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
-
+#include "DHT.h"
+// #define DHTPIN D2
+// #define DHTTYPE DHT11
+// DHT dht(DHTPIN, DHTTYPE);
+DHT dht(D2, DHT11);
 void setup()
 {
     Serial.begin(9600);
-    Serial.println(F("DHT22 TEST!"));
+    Serial.println(F("DHTxx test!"));
     dht.begin();
 }
-
 void loop()
 {
     delay(2000);
@@ -19,20 +17,22 @@ void loop()
     float f = dht.readTemperature(true);
     if (isnan(h) || isnan(t) || isnan(f))
     {
-        Serial.println(F("Failed to read from sensor!"));
+        Serial.println(F("Failed to read from DHT sensor!"));
         return;
     }
+    // Compute heat index in Fahrenheit (the default)
     float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
     float hic = dht.computeHeatIndex(t, h, false);
     Serial.print(F("Humidity: "));
     Serial.print(h);
-    Serial.print(F("%Temperature: "));
+    Serial.print(F("% Temperature: "));
     Serial.print(t);
-    Serial.print(F("'C"));
+    Serial.print(F("°C "));
     Serial.print(f);
-    Serial.print(F("F Heat Index: "));
+    Serial.print(F("°F Heat index: "));
     Serial.print(hic);
-    Serial.print(F("'C"));
+    Serial.print(F("°C "));
     Serial.print(hif);
-    Serial.print(F("'F"));
+    Serial.println(F("°F"));
 }
